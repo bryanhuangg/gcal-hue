5a959de29a167356d358f0d6aa8e530fe4e2ea2f
bryan <bryanhuang66@gmail.com>
2023-10-03 21:06:12 -0700

    color change works. bugs on month change. need to clean up code

--- content/content.js
+++ content/content.js
@@ -1,22 +1,18 @@
 // -- Inject CSS into the DOM -- //
 const eventBarClass = ".g3dbUc";
-function changeCalendarEventColor() {
-  const eventIdToSelect =
-    "MjAyMzEwMDlfN2tqdHBrdDNoZGd2Mmc3OHJ2MXIzcGkxcDAgZW4uY2FuYWRpYW4ub2ZmaWNpYWwjaG9saWRheUB2";
+
+function changeCalendarEventColor(eventId, color) {
+  console.log(eventId)
   const eventWrapperElement = document.querySelector(
-    `[data-eventid="${eventIdToSelect}"]`
+    `[data-eventid="${eventId}"]`
   );
 
   if (eventWrapperElement) {
-    const eventElement = eventWrapperElement.querySelector(eventBarClass);
+    const eventElement = eventWrapperElement.querySelector(".g3dbUc");
 
     if (eventElement) {
-      eventElement.style.backgroundColor = "orange";
-    } else {
-      console.log("Child element not found");
+      eventElement.style.backgroundColor = color;
     }
-  } else {
-    console.log("Wrapper element not found");
   }
 }
 
@@ -24,36 +20,64 @@ const colorChangeObserverConfig = {
   childList: true,
   subtree: true,
 };
-const colorChangeObserver = new MutationObserver(changeCalendarEventColor);
-colorChangeObserver.observe(document, colorChangeObserverConfig);
+const colorChangeObserver = new MutationObserver((mutationsList) => {
+  mutationsList.forEach((mutation) => {
+    // Extract the eventId from the mutation record
+    const eventId = mutation.target.getAttribute("data-eventid");
 
+    // Retrieve the color associated with this eventId from chrome.storage.sync
+    chrome.storage.sync.get(eventId, (items) => {
+      const color = items[eventId];
+      
+      if (color) {
+        changeCalendarEventColor(eventId, color);
+      }
+    });
+  });
+});
+colorChangeObserver.observe(document, colorChangeObserverConfig);
 
 
 // --- Inject button into the DOM --- //
 function injectButton() {
-    const buttonElement = document.createElement("button");
-    buttonElement.textContent = "Change Event Color";
-    buttonElement.style.margin = "5px"; 
-    buttonElement.addEventListener("click", () => {
-      changeCalendarEventColor(); 
-    });
-    const parentElement = document.querySelector('[jsname="hklcae"]');
-  
-    if (parentElement) {
-      if (!parentElement.querySelector("button")) {
-        parentElement.appendChild(buttonElement);
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = "Change Event Color";
+  buttonElement.style.margin = "5px";
+  buttonElement.addEventListener("click", (event) => {
+    // Traverse the DOM to find the event_id associated with the clicked button
+    const eventWrapperElement = findEventWrapperElement(event.target);
+
+    if (eventWrapperElement) {
+      const eventId = eventWrapperElement.getAttribute("data-eid");
+      const color = prompt("Enter a color (e.g., #FF0000):");
+      if (color) {
+        chrome.storage.sync.set({ [eventId]: color }).then(() => {
+          changeCalendarEventColor(eventId, color);
+        });
       }
-    } else {
-      console.log("Parent element not found");
+    }
+  });
+
+  const parentElement = document.querySelector('[jsname="hklcae"]');
+
+  if (parentElement) {
+    if (!parentElement.querySelector("button")) {
+      parentElement.appendChild(buttonElement);
     }
   }
-  
-  const buttonObserverConfig = {
-    childList: true,
-    subtree: true, 
-  };
-  
-  const buttonObserver = new MutationObserver(injectButton);
-  buttonObserver.observe(document, buttonObserverConfig);
+}
 
-  
+function findEventWrapperElement(element) {
+  while (element && !element.getAttribute("data-eid")) {
+    element = element.parentElement;
+  }
+  return element;
+}
+
+const buttonObserverConfig = {
+  childList: true,
+  subtree: true,
+};
+
+const buttonObserver = new MutationObserver(injectButton);
+buttonObserver.observe(document, buttonObserverConfig);
