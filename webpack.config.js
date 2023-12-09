68bad4cb01fdac1a5e8d537d4316daee45bdded4
bryan <bryanhuang66@gmail.com>
2023-12-09 15:36:37 -0800

    more bootstrapping

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,39 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    popup: './src/popup.jsx',
+    popup: "./src/popup.jsx",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
-    rules: [{ 
-        test: /\.(js|jsx)$/, 
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-            loader: 'babel-loader',
-            options: {
-                presets: ['@babel/preset-env', '@babel/preset-react']
-            }
-        } }],
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/popup.html",
+      filename: "popup.html",
+      chunks: ["popup"],
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "public" },
+      ],
+    }),
+  ],
 };
