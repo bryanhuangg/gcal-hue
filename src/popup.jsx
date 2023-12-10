3936353fc1a01c8b565fac94b458bf891542bd38
bryan <bryanhuang66@gmail.com>
2023-12-10 12:40:13 -0800

    logo added, started on popupUI

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import { BlockPicker } from "react-color";
 
 function Popup() {
+  const [background, setBackground] = useState("#d9e3f0");
+
+  const handleChangeComplete = (color) => {
+    setBackground(color.hex);
+  };
+
   return (
     <div>
-      <BlockPicker/>
+      <img
+        src={chrome.runtime.getURL("logo.png")}
+        alt="Logo"
+        style={{ width: "60px" }}
+      />
+
+
+      <BlockPicker color={background} onChangeComplete={handleChangeComplete} />
     </div>
   );
 }
