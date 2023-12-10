36ade7e677ae4f38eb79abd74d2702890b5aa90e
bryan <bryanhuang66@gmail.com>
2023-12-10 15:22:31 -0800

    improved on popup

--- src/popup.jsx
+++ src/popup.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
 import { BlockPicker } from "react-color";
+import { CloseButton } from "@chakra-ui/close-button";
+import {
+  ChakraProvider,
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Switch,
+} from "@chakra-ui/react";
+import { IoMdSettings, IoMdColorPalette } from "react-icons/io";
 
 function Popup() {
   const [background, setBackground] = useState("#d9e3f0");
@@ -9,17 +20,62 @@ function Popup() {
     setBackground(color.hex);
   };
 
+  const handleClose = () => {
+    window.close();
+  };
+
   return (
-    <div>
-      <img
-        src={chrome.runtime.getURL("logo.png")}
-        alt="Logo"
-        style={{ width: "60px" }}
-      />
+    <ChakraProvider>
+      <div style={{ width: "200px" }}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <img
+            src={chrome.runtime.getURL("logo.png")}
+            alt="Logo"
+            style={{ width: "60px", margin: "10px" }}
+          />
 
+          <CloseButton
+            onClick={handleClose}
+            size="sm"
+            border="none"
+            _hover={{ cursor: "pointer" }}
+            color="light-grey"
+            style={{
+              borderRadius: "50%",
+              backgroundColor: "transparent",
+              margin: "10px",
+            }}
+          />
+        </div>
 
-      <BlockPicker color={background} onChangeComplete={handleChangeComplete} />
-    </div>
+        <Tabs align="center" variant="enclosed" size="sm">
+          <TabList>
+            <Tab> <IoMdColorPalette style={{margin: "2px"}}/> Colors </Tab>
+            <Tab> <IoMdSettings  style={{margin: "2px"}}/> Settings </Tab>
+          </TabList>
+          <TabPanels>
+            {/* Color Pallete */}
+            <TabPanel>
+              <BlockPicker
+                color={background}
+                onChangeComplete={handleChangeComplete}
+              />
+            </TabPanel>
+
+            {/* Settings */}
+            <TabPanel>
+              Enable colour overlay?
+              <Switch size="sm" style={{marginLeft:'10px'}}/>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
+    </ChakraProvider>
   );
 }
 
